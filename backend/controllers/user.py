b722995fac1be1ee86f7dba5ca832b3753eb53cd
baby-0105 <epifa_kikka@icloud.com>
2022-11-06 14:37:35 +0900

    gomi

--- backend/controllers/user.py
+++ backend/controllers/user.py
@@ -6,7 +6,6 @@ class UsersController:
     def get_name(cls, db):
         try:
             user = db.query(UsersModel).filter(UsersModel.id == 1).first()
-            print(user)
             return user
         except:
             return {"error"}
