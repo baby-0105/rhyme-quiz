47531982e6a7c43d8ab88d490ffb03b898c12882
baby-0105 <epifa_kikka@icloud.com>
2022-11-12 01:04:47 +0900

    Composition API に書き換え

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,18 @@
-<script>
-export default {
-  data() {
-    return {
-      users: ""
-    }
-  },
-  methods: {
-    getUser() {
-      this.axios
-        .get("/users")
-        .then((res) => {
-          this.users = res.data
-        })
-        .catch((e) => {
-          console.log('e:', e);
-        })
-    }
-  }
+<script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+
+let userName = ref<string>("")
+
+function getUser() {
+  axios
+    .get("/users")
+    .then((res) => {
+      userName.value = res.data
+    })
+    .catch((e) => {
+      console.log('e:', e);
+    })
 }
 </script>
 
@@ -24,7 +20,7 @@ export default {
   <div class="text-center">
     <p class="text-rose-500 text-3xl">Hello World</p>
     <button class="border p-1" @click="getUser" type="button">DBからユーザー情報取得</button>
-    <div>{{ users }}</div>
+    <div>{{ `ユーザー名：${userName}` }}</div>
   </div>
 </template>
 
