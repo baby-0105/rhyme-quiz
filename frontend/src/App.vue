f6d619adcfbd992ab70487adc792638a2c7c9887
baby-0105 <epifa_kikka@icloud.com>
2022-11-11 23:49:13 +0900

    vue-cli からviteへ変更

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,9 @@
-<template>
-  <div id="app">
-    <div class="main container">
-      <router-view/>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-}
+<script setup lang="ts">
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-.main {
-  padding-top: 5em;
-}
+<template>
+  <h1>App.vue</h1>
+</template>
+
+<style scoped>
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import '@/assets/style/tailwind-input.css';
+// import '@/assets/style/tailwind-input.css';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import router from './router';
 
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:8888/';
 
 const app = createApp(App)
-app.use(router).use(VueAxios, axios).mount('#app')
+app.use(VueAxios, axios).mount('#app')
